665a75a9c339a5843cd124d19365af3bb0f64289
whoiszxl <whoiszxl@gmail.com>
2021-09-09 08:15:28 +0800

    add flutter app

---
+++ flutter_video_app/flutter_video_app/lib/api/api.dart
@@ -0,0 +1,7 @@
+class Api {
+  //基础url
+  static final String baseUrl = "http://localhost:80";
+
+  static final String homeBanners = baseUrl + "/home/banners";
+
+}
---
+++ flutter_video_app/flutter_video_app/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:provide/provide.dart';
+
+void main() {
+
+
+  return runApp(ProviderNode(
+    child: MyApp(),
+    providers: null,
+  ));
+}
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
